c3c238311db9a8744af68c84e9e09a39766da7f3
George Carter <george.carter@meyertool.com>
2016-02-15 15:58:27 -0500

    DrawingClassTest for Minesweeper

---
+++ MineR-master/src/DrawingClassTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.awt.*;
+import java.awt.event.InputEvent;
+import javax.swing.JButton;
+
+/**
+ * Created by george.carter on 2/15/2016.
+ */
+public class DrawingClassTest {
+    @Test
+    public void testRightClickButtonEnabled() throws Exception
+    {
+        DrawingClass drw = new DrawingClass();
+        drw.setAll();
+
+        Robot robot = new Robot();
+        robot.mouseMove(30, 50);
+        robot.mousePress(InputEvent.BUTTON3_MASK);
+        robot.mouseRelease(InputEvent.BUTTON3_MASK);
+
+        try { Thread.sleep(2000); } catch(InterruptedException ex) { Thread.currentThread().interrupt(); }
+
+        JButton b[] = drw.getClicks();
+        assertEquals(false, b[0].isEnabled());
+    }
+    @Test
+    public void testRightClickButtonColor() throws Exception
+    {
+        DrawingClass drw = new DrawingClass();
+        drw.setAll();
+
+        Robot robot = new Robot();
+        robot.mouseMove(30, 50);
+        robot.mousePress(InputEvent.BUTTON3_MASK);
+        robot.mouseRelease(InputEvent.BUTTON3_MASK);
+
+        try { Thread.sleep(2000); } catch(InterruptedException ex) { Thread.currentThread().interrupt(); }
+
+        JButton b[] = drw.getClicks();
+        assertEquals(Color.GREEN, b[0].getBackground());
+    }
+}
